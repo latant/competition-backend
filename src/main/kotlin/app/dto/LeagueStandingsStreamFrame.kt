e3e3cb611f3210a25582eeb32f0ed20fc1c9c6c1
Latinovits Antal <latinovitsantal@gmail.com>
2020-11-22 20:53:39 +0100

    Remove unnecessary dto classes

--- src/main/kotlin/app/dto/LeagueStandingsStreamFrame.kt
+++
@@ -1,9 +0,0 @@
-package app.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class LeagueStandingsStreamFrame(
-    val name: String,
-    val standingsTable: StandingsTable,
-)

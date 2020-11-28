fd62aeaa8284be3e160b238df803dd67e948d7dc
Latinovits Antal <latinovitsantal@gmail.com>
2020-11-28 12:24:32 +0100

    Configure CORS

--- src/main/kotlin/app/security/cors.kt
+++ src/main/kotlin/app/security/cors.kt
@@ -1,18 +1,9 @@
 package app.security
 
 import io.ktor.features.*
-import io.ktor.http.HttpHeaders.Authorization
-import io.ktor.http.HttpMethod.Companion.Delete
-import io.ktor.http.HttpMethod.Companion.Get
-import io.ktor.http.HttpMethod.Companion.Head
-import io.ktor.http.HttpMethod.Companion.Options
-import io.ktor.http.HttpMethod.Companion.Patch
-import io.ktor.http.HttpMethod.Companion.Post
-import io.ktor.http.HttpMethod.Companion.Put
 
 fun CORS.Configuration.configureCORS() {
     anyHost()
+    allowNonSimpleContentTypes = true
     allowCredentials = true
-    headers += setOf(Authorization)
-    methods += setOf(Get, Put, Patch, Post, Delete, Head, Options)
 }

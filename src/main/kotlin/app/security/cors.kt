bb720a60b0eaed794fa0dd3e62eee0f711275330
Latinovits Antal <latinovitsantal@gmail.com>
2020-11-29 10:41:39 +0100

    Improve CORS

--- src/main/kotlin/app/security/cors.kt
+++ src/main/kotlin/app/security/cors.kt
@@ -1,9 +1,21 @@
 package app.security
 
 import io.ktor.features.*
+import io.ktor.http.*
+import io.ktor.http.HttpMethod.Companion.Delete
+import io.ktor.http.HttpMethod.Companion.Get
+import io.ktor.http.HttpMethod.Companion.Head
+import io.ktor.http.HttpMethod.Companion.Options
+import io.ktor.http.HttpMethod.Companion.Patch
+import io.ktor.http.HttpMethod.Companion.Post
+import io.ktor.http.HttpMethod.Companion.Put
 
 fun CORS.Configuration.configureCORS() {
     anyHost()
+    methods += setOf(Patch, Post, Put, Options, Get, Head, Delete)
+    header(HttpHeaders.Authorization)
     allowNonSimpleContentTypes = true
     allowCredentials = true
+    allowXHttpMethodOverride()
+    allowSameOrigin
 }
